13109d6c5e4a49f153a8e2fa0f5e052152a3e55c
Karol Michalak <karol.michalak93@gmail.com>
2019-01-26 14:41:31 +0100

    Structuring the app

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class BurgerBuilder extends Component {
+    render() {
+        return(
+            <>
+                <div>Burger</div>
+                <div>Builds Controls</div>
+            </>
+        );
+    }
+}
+
+export default BurgerBuilder;
